5636ba140a2502f1b8afdbfe8bdb98a03cc02ebe
neskovicd <neskovicdragan@hotmail.com>
2021-02-05 17:49:43 +0100

    alpha build

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroAboutUs from "../components/HeroAboutUs"
-import OurVision from "../components/Ourvision"
+import OurVision from "../components/OurVision"
 import Team from "../components/Team"
 
 const Aboutus = () => (
